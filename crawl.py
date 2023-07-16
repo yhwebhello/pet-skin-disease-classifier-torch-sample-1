160c8a3db322d63223d3511e06c2e9fc42bb50f9
ShinHyun-soo <69250097+ShinHyun-soo@users.noreply.github.com>
2023-07-16 17:44:38 +0900

    add img

--- crawl.py
+++ crawl.py
@@ -40,7 +40,7 @@ def get_image_urls(search_query, num_images):
     return image_urls[:num_images]
 
 # 이미지 다운로드 예시
-search_query = "dog allergic dermatitis img"
+search_query = "dog hot spot img"
 num_images = 100
 image_urls = get_image_urls(search_query, num_images)
 
