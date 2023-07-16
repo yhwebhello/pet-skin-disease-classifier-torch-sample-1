5e68555c6113b3cf3c49a2267dd7e8407f62cf21
ShinHyun-soo <69250097+ShinHyun-soo@users.noreply.github.com>
2023-07-16 18:47:14 +0900

    Add augmented imgs
    Add augmented imgs.

--- agumentation.py
+++ agumentation.py
@@ -3,7 +3,7 @@ import imgaug.augmenters as iaa
 import cv2
 
 # 데이터 폴더 경로 설정
-folder_path = "./Yeast infection"  # 이미지 파일들이 있는 폴더 경로를 지정하세요
+folder_path = "./Impetigo"  # 이미지 파일들이 있는 폴더 경로를 지정하세요
 
 # 이미지 증강 기법 정의
 augmenter = iaa.Sequential([
